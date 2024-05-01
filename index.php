e19f72028eb101011c4234f0eb2c834d6b33f846
alexramosiii-dev <142112454+alexramosiii-dev@users.noreply.github.com>
2024-05-02 01:03:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -54,6 +54,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  */
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 
 /*
  *---------------------------------------------------------------
